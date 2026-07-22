8a6eb91a49e16095df9155144f71947464d06679
tazacortea <tazacortea@gmail.com>
2026-07-22 14:44:44 +0100

    latest version

---
+++ react.vite-/src/About-page.jsx
@@ -0,0 +1,11 @@
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page of our application.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ react.vite-/src/Home-page.jsx
@@ -0,0 +1,11 @@
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page of our application.</p>
+    </div>
+  );
+}
+
+export default HomePage;
