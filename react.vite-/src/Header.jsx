bec38028d52bb0d7f1002c03bc50c9acd7a6568e
tazacortea <tazacortea@gmail.com>
2026-07-22 14:34:06 +0100

    22222222

---
+++ react.vite-/src/Header.jsx
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+
+<body>
+    
+      
+   <div className="header">
+     <h1>LOGO</h1>
+     <div className="nav">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>   
+        </ul>
+        <button>Login</button>
+     </div>
+   </div>                
+    
+</body>
+</html>
